8e5657f77f2091f74994d04095085f4e9ef363e4
Jake O'Donnell <jakeod99@gmail.com>
2018-05-17 05:14:17 -0400

    Changed URL for signin_signin to /signin instead of /signin/signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'signin/signin'
+  get 'signin' => 'signin#signin', :as => :signin_signin
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
