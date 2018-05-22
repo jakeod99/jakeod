392ca1d889d11e2b1273eb18038a70709b0dd792
Jake O'Donnell <jakeod99@gmail.com>
2018-05-22 03:06:57 -0400

    Generated migration with is_admin boolean for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_050236) do
+ActiveRecord::Schema.define(version: 2018_05_22_070354) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_05_16_050236) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
   end
 
 end
