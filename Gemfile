7e8860a4f5b78692ee3a1eb004501bcf32f077ea
Jake O'Donnell <jakeod99@gmail.com>
2018-05-23 04:03:09 -0400

    Added validations (including password validation from NOBSPW) to User

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'rails', '~> 5.2.0'
 gem 'bcrypt', '~> 3.1.12'
 # Use NOBSPW for password requirements
 gem 'nobspw'
-# Use sqlite3 as the database for Active Record
+# Use sqlite3 (the one from github that works) as the database for Active Record
 gem 'sqlite3', :git => 'https://github.com/sparklemotion/sqlite3-ruby'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,32 @@
 class User < ApplicationRecord
+
+  # email validation
   before_save { self.email = email.downcase}
-  validates :name, presence: true, length: { maximum: 50 }
   EMAIL_VALIDATION_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 50 },
             format: { with: EMAIL_VALIDATION_REGEX },
             uniqueness: { case_sensitive: false }
+
+
+  # password validation
+  # removes password_too_common validation from NOBSPW validations
+  NOBSPW.configuration.validation_methods.delete(:password_too_common?)
+
+  # bcrypt gem uses this.
+  # User has password and password_confirmation fields
+  # users table has encrypted password_digest column
   has_secure_password
+
+  # this runs NOBSPW validation. username => name b/c User's username is name
+  validates :password, password: { :username => :name},
+            if: -> { new_record? || changes[:password] }
+
+
+  # name validation
+  validates :name, presence: true, length: { maximum: 50 }
+
+
+  #
+  validates :is_admin, presence: true
+
 end
