e5e8445a571694bca4a85704a58a2e74b96fc238
Jake O'Donnell <jakeod99@gmail.com>
2018-06-01 03:12:21 -0400

    Added bare registration html and fixed tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find_by(name: params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
   end
 end
--- test/controllers/user_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UserControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get user_register_url
-    assert_response :success
-  end
-
-end
