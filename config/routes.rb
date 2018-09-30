6d8104eef3d8e3bb2a427060765935dc2b40fb36
Jake O'Donnell <jakeod99@gmail.com>
2018-09-30 02:09:09 -0400

    Replaced routes to unfinished features with new construction page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
-  delete '/signout', to: 'sessions#destroy'
-  get '/register', to: 'users#new'
-  resources :users
+
+  # uncomment to resume work on user sessions
+  # get 'error/construction'
+  # get '/signin', to: 'sessions#new'
+  # post '/signin', to: 'sessions#create'
+  # delete '/signout', to: 'sessions#destroy'
+  # get '/register', to: 'users#new'
+  # resources :users
+
+  # delete when starting work on user sessions
+  get '/signin', to: 'error#construction'
+  get '/register', to: 'error#construction'
+  get '/users', to: 'error#construction'
+
 end
